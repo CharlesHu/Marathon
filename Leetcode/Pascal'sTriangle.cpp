40dfde473fb6d5d65a4769da4976274447b5c289
Charles Hu <twolittlecats@163.com>
2014-11-03 12:31:23 +0800

    1

--- Leetcode/Pascal'sTriangle.cpp
+++ Leetcode/Pascal'sTriangle.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
-    vector<int> getRow(int rowIndex) {
-        vector<int> ans;
-        for (long long i = 0, j = 1; i <= rowIndex; ++i, j = j*(rowIndex-i+1)/i) ans.push_back(j);
+    vector<vector<int> > generate(int numRows) {
+        vector<vector<int> > ans;
+        vector<int> v;
+        for (int i = 0; i < numRows; ++i) {
+            v.clear();
+            for (long long j = 0, t = 1; j <= i; ++j, t = t*(i-j+1)/j) {
+                v.push_back(t);
+            }
+            ans.push_back(v);
+        }
         return ans;
     }
 };
